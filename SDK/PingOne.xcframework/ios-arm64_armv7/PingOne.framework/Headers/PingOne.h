0f7e87e51e2762f14b8f8fd3fa84858698f6405e
Asaf David <adavid@pingidentity.com>
2020-10-29 16:33:49 +0200

    v1.3.1a

---
+++ SDK/PingOne.xcframework/ios-arm64_armv7/PingOne.framework/Headers/PingOne.h
@@ -0,0 +1,19 @@
+//
+//  PingOne.h
+//  PingOne
+//
+//  Created by Ping Identity on 3/12/19.
+//  Copyright © 2019 Ping Identity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PingOne.
+FOUNDATION_EXPORT double PingOneVersionNumber;
+
+//! Project version string for PingOne.
+FOUNDATION_EXPORT const unsigned char PingOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PingOne/PublicHeader.h>
+
+
