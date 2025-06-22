31ef8fa3a3097407dfd9d924005ab04512187eb8
yizharkatif <138602732+yizharkatif@users.noreply.github.com>
2025-06-22 12:40:41 +0300

    version 2.1.0
    Added support for the PingOne Singapore geography

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,11 +12,16 @@ let package = Package(
             targets: ["PingOneSDK"]
         )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/airsidemobile/JOSESwift.git", exact: "3.0.0"),
+        .package(url: "https://github.com/ashleymills/Reachability.swift", exact: "5.2.4"),
+        .package(url: "https://github.com/lachlanbell/SwiftOTP.git", exact: "3.0.2"),
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", exact: "0.9.19")
+    ],
     targets: [
         .binaryTarget(
             name: "PingOneSDK",
-            url: "https://assets.pingone.com/pingonemobile/ios-sdk/release/PingOneSDK.xcframework.2.0.0.zip",
-            checksum: "b6ad702a800eff9b00a27cc6889bf13e413b66a45044d5a635a56551f470b5a8")
+            url: "https://assets.pingone.com/pingonemobile/ios-sdk/release/PingOneSDK.xcframework.2.1.0.zip",
+            checksum: "e447ee44395e9e347d0fd65e7d4bafc75e966a96a791aeae9b53463e7473ffad")
     ]
 )
